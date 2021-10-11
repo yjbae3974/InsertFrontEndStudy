48d31ed000dade005d4fa6fd2aaf8224749a7a3d
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2021-10-11 14:12:10 +0900

    bootstrap

--- 심하민/bootstrap/app.js
+++ 심하민/bootstrap/app.js
@@ -1,10 +1,29 @@
 const right = document.querySelector(".right")
-const link = right.querySelectorAll("a")
-console.log(right)
-console.log(link)
-
-for (i=0;i<link.length;i++){
-    link[i].addEventListener("click", (e)=>{
-        e.style = "color: white"
-    });
-}
+const home = right.querySelector(".home")
+const feature = right.querySelector(".feature")
+const contact = right.querySelector(".contact")
+console.log(home)
+home.addEventListener("click",(e)=>{
+    feature.style.color ="rgb(148,148,148)"
+    contact.style.color ="rgb(148,148,148)"
+    // feature.style.borderBottom="2px solid black"
+    // contact.style.borderBottom="2px solid black"
+    e.path[0].style.color = "white";
+    e.path[0].style.borderBottom ="2px solid rgb(148,148,148)";
+});
+feature.addEventListener("click",(e)=>{
+    home.style.color="rgb(148,148,148)"
+    contact.style.color="rgb(148,148,148)"
+    // home.style.borderBottom="2px solid black"
+    // contact.style.borderBottom="2px solid black"
+    e.path[0].style.color = "white";
+    e.path[0].style.borderBottom="2px solid rgb(148,148,148)";
+});
+contact.addEventListener("click",(e)=>{
+    feature.style.color="rgb(148,148,148)"
+    home.style.color="rgb(148,148,148)"
+    // feature.style.borderBottom="2px solid black;"
+    // home.style.borderBottom="2px solid black"
+    e.path[0].style.color = "white";
+    e.path[0].style.borderBottom="2px solid rgb(148,148,148)";
+});
